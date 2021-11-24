bbe3a2acf94ee9032820bdc9100e53625f2125bd
Karl MARQUES <kmarques@vetixy.com>
2021-11-24 01:17:24 +0100

    fix unit tests

--- api/__tests__/e2e/user.spec.js
+++ api/__tests__/e2e/user.spec.js
@@ -1,46 +1,46 @@
-const request = require('supertest');
-const app = require('../../index');
-const { sequelize } = require('../../models/User');
+const request = require("supertest");
+const app = require("../../index");
+const { sequelize } = require("../../models/User");
 
 const bodyObject = {
-  email: 'tesst',
-  password: 'test',
-  lastname: 'test',
-  firstname: 'test',
+  email: "tesst@test.com",
+  password: "test",
+  lastname: "test",
+  firstname: "test",
 };
 
 async function createUser() {
   const res = await request(app)
-    .post('/users')
-    .set('Content-type', 'application/json')
+    .post("/users")
+    .set("Content-type", "application/json")
     .send(bodyObject);
 
   return res.body;
 }
 
-describe('Post Endpoints', () => {
+describe("Post Endpoints", () => {
   beforeEach(async () => {
     await sequelize.query('DELETE FROM "Users"');
   });
   afterAll(async () => {
     await sequelize.close();
   });
-  it('should get users', async () => {
-    const res = await request(app).get('/users');
+  it("should get users", async () => {
+    const res = await request(app).get("/users");
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual([]);
   });
 
-  it('should create users', async () => {
+  it("should create users", async () => {
     const res = await request(app)
-      .post('/users')
-      .set('Content-type', 'application/json')
+      .post("/users")
+      .set("Content-type", "application/json")
       .send(bodyObject);
     expect(res.statusCode).toEqual(201);
     expect(res.body).toEqual(expect.objectContaining(bodyObject));
   });
 
-  it('should return one user', async () => {
+  it("should return one user", async () => {
     const user = await createUser();
 
     const res = await request(app).get(`/users/${user.id}`);
@@ -48,24 +48,24 @@ describe('Post Endpoints', () => {
     expect(res.body).toEqual(user);
   });
 
-  it('should update one user', async () => {
+  it("should update one user", async () => {
     const user = await createUser();
 
     const res = await request(app)
       .put(`/users/${user.id}`)
-      .set('Content-type', 'application/json')
+      .set("Content-type", "application/json")
       .send({
-        firstname: 'foo',
+        firstname: "foo",
       });
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual(
       expect.objectContaining({
-        firstname: 'foo',
-      }),
+        firstname: "foo",
+      })
     );
   });
 
-  it('should delete one user', async () => {
+  it("should delete one user", async () => {
     const user = await createUser();
 
     const res = await request(app).delete(`/users/${user.id}`);
